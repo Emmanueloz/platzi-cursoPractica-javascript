8528c3b5ee77ff6162080443dcf4bf7688374ec0
Emmanueloz <davidozuna84@hotmail.com>
2022-07-02 14:35:05 -0500

    Calulado el triangulo desde la pagina

--- figuras.js
+++ figuras.js
@@ -13,6 +13,33 @@ bottomCuadrado.addEventListener("click", calculoCuadrado);
 
 let resultadoCuadrado = document.getElementById("resultadoCuadrado");
 
+// Código triangulo
+function calculaTriangulo() {
+    let ld1 = document.getElementById("lado1");
+    let lado1 = parseInt(ld1.value);
+    let ld2 = document.getElementById("lado2");
+    let lado2 = parseInt(ld2.value);
+    let ba = document.getElementById("base");
+    let base = parseInt(ba.value);
+    let perimetryTriangulo = lado1 + lado2 + base;
+    let resultadoArea = areaTriangulo(lado1, lado2, base);
+    console.log(`Perímetro: ${perimetryTriangulo }, area: ${resultadoArea}`);
+    resultadoTriangulo.innerHTML = `El perímetro del triangulo es: <b>${perimetryTriangulo}</b> 
+    <br> El area del triangulo es: <b>${resultadoArea}</b>`;
+}
+
+function areaTriangulo(ladoA, ladoB, ladoC) {
+    let S = (ladoA + ladoB + ladoC) / 2;
+    let multiplication = S * (S -ladoA) * (S - ladoB) * (S - ladoC);
+    let resultado = Math.sqrt(multiplication);
+    return resultado;
+}
+
+let bottomTriangulo = document.getElementById("calculaTriangulo");
+bottomTriangulo.addEventListener("click", calculaTriangulo);
+
+let resultadoTriangulo = document.getElementById("resultadoTriangulo")
+
 /* console.group("Cuadrado")
 const ladoCuadrado = 5;
 
