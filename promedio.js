b86f3f2b7d5b0f5983da394048a6d53bd12e7236
Emmanueloz <davidozuna84@hotmail.com>
2022-08-07 10:26:25 -0500

    Calcular promedio y usar la pagina de html

--- promedio.js
+++ promedio.js
@@ -1,5 +1,49 @@
+// Lista vacía
+let arrayLista = [];
+let listaArray = document.getElementById("listaArray");
+let resultPromedio = document.getElementById("resultPromedio")
 
+// Calcular promedio
+function calculaMediaArithmetic(lista) {
+  const sumaLista = lista.reduce(
+    (valorAcumulado = 0, nuevoElemento) => {
+      return valorAcumulado + nuevoElemento;
+    }
+  )
+  
+  console.log(sumaLista);
+  const promedio = sumaLista / lista.length;
+  return promedio;
+}
 
-function calcularPromedio(){
-  alert("hola");
+// Agrega elemento al array
+function agregarEnArray(nuevoValor, lista) {
+  return lista.push(nuevoValor);
+}
+
+// Obtener nuevo valor al array y mostrar en documento
+function agregarValor() {
+  let input = document.getElementById("input");
+  let inputValue = parseInt(input.value);
+
+  if (isNaN(inputValue)) {
+    listaArray.innerHTML = "<b style='color:red'>Coloca numero y no otro valor</b>"
+  }
+  else {
+    agregarEnArray(inputValue, arrayLista);
+    console.log(arrayLista)
+    for (lis of arrayLista) {
+      listaArray.innerHTML = `<li>${arrayLista}</li>`;
+    } 
+  }
+}
+
+// Calcular promedio y mostrar en el documento
+function calcularPromedio() {
+  /* let input = document.getElementById("input");
+  let inputValue = input.value; */
+
+  let promedio = calculaMediaArithmetic(arrayLista);
+  console.log(promedio);
+  resultPromedio.innerHTML = `El promedio es de ${promedio}`;
 }
