b902e1282fa1deafe190f3b47750726add925ee4
Emmanueloz <davidozuna84@hotmail.com>
2022-10-13 14:08:00 -0500

    Agregando tema oscura

--- analisis.js
+++ analisis.js
@@ -1,14 +1,16 @@
 // Helpers
+const ArrayCountry = []
+
+let salarioCountry = document.getElementById("salario")
+
 function esPar(number) {
-  return (number % 2 === 0);
+  return number % 2 === 0;
 }
 
 function calculaMediaArithmetic(lista) {
-  const sumaLista = lista.reduce(
-    (valorAcumulado = 0, nuevoElemento) => {
-      return valorAcumulado + nuevoElemento;
-    }
-  )
+  const sumaLista = lista.reduce((valorAcumulado = 0, nuevoElemento) => {
+    return valorAcumulado + nuevoElemento;
+  });
   const promedio = sumaLista / lista.length;
   return promedio;
 }
@@ -21,38 +23,38 @@ function medianaSalarios(lista) {
   if (esPar(lista.length)) {
     const personMitad01 = lista[mitad - 1];
     const personMitad02 = lista[mitad];
-    const mediana = calculaMediaArithmetic([personMitad01, personMitad02])
+    const mediana = calculaMediaArithmetic([personMitad01, personMitad02]);
     return mediana;
   } else {
     const personMitad = lista(mitad);
-    return personMitad
+    return personMitad;
   }
 }
 
-// Mediana General
-const salariosCol = Colombia.map(
-  (person) => {
-    return person.salary;
-  }
-);
+function agregarSalario() {
+  let salarioCountryValue = parseInt(salarioCountry.value)
+  ArrayCountry.push(salarioCountryValue)
+}
 
-const salariosColSorted = salariosCol.sort(
-  (salaryA, SalaryB) => {
+function CalarSalarios() {
+  // Mediana General
+  const salarios = ArrayCountry.map((person) => {
+    return person;
+  });
+
+  const salariosSorted = salarios.sort((salaryA, SalaryB) => {
     return salaryA - SalaryB;
-  }
-);
+  });
 
-const medianaGeneralCol = medianaSalarios(salariosColSorted)
+  const medianaGeneral = medianaSalarios(salariosSorted);
 
-// Mediana del top 10%
-const spliceStart = (salariosColSorted.length * 90) / 100;
-const spliceCount = salariosCol.length - spliceStart;
+  // Mediana del top 10%
+  const spliceStart = (salariosSorted.length * 90) / 100;
+  const spliceCount = salarios.length - spliceStart;
 
-const salariosColTop10 = salariosColSorted.splice(
-  spliceStart,
-  spliceCount
-);//Se le cola la posición en donde queremos que empiece a cortar, cuanto valores queremos que devuelva
+  const salariosTop10 = salariosSorted.splice(spliceStart, spliceCount); //Se le cola la posición en donde queremos que empiece a cortar, cuanto valores queremos que devuelva
 
-const medianaTop10Col = medianaSalarios(salariosColTop10);
+  const medianaTop10 = medianaSalarios(salariosTop10);
 
-console.log(medianaGeneralCol, medianaTop10Col);
+  console.log(medianaGeneral, medianaTop10);
+}
