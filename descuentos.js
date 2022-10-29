ed246b5930be3831b9c9482ea6d202fa0b45ede7
Emmanueloz <davidozuna84@hotmail.com>
2022-10-28 20:29:04 -0500

    Problemas de sintaxis en descuentos solucionado

--- descuentos.js
+++ descuentos.js
@@ -1,4 +1,5 @@
 const resultP = document.getElementById("ResultP");
+const resultPC = document.getElementById("ResultPC");
 
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajePrecioConDescuento = 100 - descuento;
@@ -13,16 +14,16 @@ function onClickButtonPriceDiscount() {
 
   const inputDiscount = document.getElementById("InputDiscount");
   const discountValue = parseInt(inputDiscount.value);
-  
+
   if (isNaN(priceValue) || isNaN(discountValue)) {
-    resultP.innerHTML = "<b style='color: #d43131'>Error coloque Números en los campos que lo pide</b>"
-  }
-  else {
+    resultP.innerHTML =
+      "<b style='color: #d43131'>Error coloque Números en los campos que lo pide</b>";
+  } else {
     const precioConDescuento = calcularPrecioConDescuento(
       priceValue,
       discountValue
     );
-  
+
     resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
   }
 }
@@ -76,48 +77,46 @@ function aplicarDescuento(userCoupon) {
       descuento = 10;
       break;
   }
-  return descuento
+  return descuento;
 }
 
 function comparison(userCoupon, arrayCopones) {
   let validation = comparisonBucle(userCoupon, arrayCopones);
 
   if (userCoupon === validation) {
-    const descuento = aplicarDescuento(userCoupon)
+    const descuento = aplicarDescuento(userCoupon);
     return descuento;
   } else {
-    let message = "Cupón invalido"
+    let message = "Cupón invalido";
     return message;
   }
 }
 
-function onClickButtonPriceDiscountCoupon(){
+function onClickButtonPriceDiscountCoupon() {
   const inputPrice = document.getElementById("InputPriceCoupon");
   const priceValue = parseInt(inputPrice.value);
+  console.log(priceValue);
 
   const inputDiscount = document.getElementById("InputCoupon");
   const discountValue = inputDiscount.value;
 
-  const resultP = document.getElementById("ResultPC");
-
   const discount = comparison(discountValue, cupones);
+  console.log(discount);
   let precioConDescuento;
 
-  let parseIntDiscount = typeof(discount)
-  if (parseIntDiscount === "parseInt" && !isNaN(priceValue)) {
+  let parseIntDiscount = typeof discount;
+  if (parseIntDiscount === "number" && !isNaN(priceValue)) {
     precioConDescuento = calcularPrecioConDescuento(priceValue, discount);
-    //console.log(precioConDescuento);
-    resultP.innerHTML = `El precio con descuento por tu cupón es: <b>$${precioConDescuento}</b>`;
+    console.log(precioConDescuento);
+    resultPC.innerHTML = `El precio con descuento por tu cupón es: <b>$${precioConDescuento}</b>`;
+  } else if (isNaN(priceValue) && parseIntDiscount !== "number") {
+    console.log("error");
+    resultPC.innerHTML = `<b style="color:#d43131">Error: ${discount}, y coloque números en el campo correspondiente</b>`;
+  } else if (isNaN(priceValue)) {
+    console.log("error");
+    resultPC.innerHTML = `<b style="color:#d43131">Error: Coloque un numero en el campo correspondiente</b>`;
+  } else {
+    console.log("error");
+    resultPC.innerHTML = `<b style="color:#d43131" >Error: ${discount}</b>`;
   }
-  else if (isNaN(priceValue) && parseIntDiscount !== "parseInt") {
-    resultP.innerHTML = `<b style='color:#d43131>Error: ${discount}, y coloque números en el campo correspondiente</b>`
-  }
-  else if (isNaN(priceValue)) {
-    resultP.innerHTML = "<b style='color:#d43131'>Error: Coloque un numero en el campo correspondiente</b>"
-  }
-  else {
-    //console.log(discount);
-    resultP.innerHTML = `<b style="color:#d43131 >Error: ${discount}</b>`
-  }
-
 }
